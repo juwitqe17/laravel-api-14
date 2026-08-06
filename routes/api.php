e5969ff51372f2b4bf9bab01412ca12c5092ac7c
Juwita Serina <juwitaserina0@gmail.com>
2026-08-06 07:40:10 +0700

    add delete method

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/product', [ProductController::class, 'index'])->name('product');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
-Route::put('/product{product}', [ProductController::class, 'update'])->name('product.update');
+Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
 
 //Route::apiResource('/products', ProductController::class);
